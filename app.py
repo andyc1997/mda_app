f8f16edd5e5be8c2acfaa13182eed2f2957ad085
waichun cheung <waichun622@hotmail.com>
2021-05-29 02:35:29 +0200

    heroku application

---
+++ app.py
@@ -0,0 +1,219 @@
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+
+import pandas as pd
+from graphs import *
+import dill
+
+# Read multiconutries data
+heatdays = pd.read_csv('europe_heatdays.csv')
+heatdays = subset_helper(heatdays).subset()
+visuals = overall_plots(heatdays)
+
+# Read model
+with open('gradientboosting.pkl', 'rb') as model_file:
+    model = dill.load(model_file)
+
+
+app = dash.Dash(__name__,title = "Visualisations", 
+                external_stylesheets = [dbc.themes.JOURNAL])
+server = app.server
+# Adding dropdown box for selecting countries
+dropdown = dcc.Dropdown(id = "id_country",
+                        options = [
+                            {"label": 'Belgium', 'value': 'Belgium'},
+                            {"label": 'Bulgaria', 'value': 'Bulgaria'},
+                            {"label": 'France', 'value': 'France'},
+                            {"label": 'Greece', 'value': 'Greece'},
+                            {"label": 'Romania', 'value': 'Romania'},
+                            {"label": 'Russia', 'value': 'Russia'},
+                            {"label": 'United Kingdom', 'value': 'UK'},
+                        ],
+                        value = 'Belgium')
+
+# Slider for meteorological variables, avoid extrapolation
+data_pred = {'TEMP_MEAN': 21, 'TEMP_RNG': 15, 'WS50M_MEAN': 5.76, 'PRECTOT_MEAN': 2.18, 
+             'RH2M_MEAN': 76.51, 'HEAT_DAYS': 2, 'YEAR': 2018, 'MONTH': 8, 'REGION': 4000,
+             'COD': 'Diseases of the circulatory system'}
+
+slider1 = dcc.Slider(id = 'id_temp_mean',
+    min = 14,
+    max = 24,
+    value = 21,
+    step = 0.01,
+    marks = {i: {'label': '{}°C'.format(i)} for i in range(14, 25)},
+    updatemode = 'drag')
+
+slider2 = dcc.Slider(id = 'id_temp_rng',
+    min = 11,
+    max = 16,
+    value = 15,
+    step = 0.01,
+    marks = {i: {'label': '{}°C'.format(i)} for i in range(11, 17)})
+
+slider3 = dcc.Slider(id = 'id_ws50m_mean',
+    min = 5.00,
+    max = 6.50 + 1 / 100**5, # Bug in dash applications for floating dict
+    value = 5.76,
+    step = 0.01,
+    marks = {i / 100 + 1 / 100**5: {'label': '{}m/s'.format(i / 100)} for i in range(500, 660, 25)})
+
+slider4 = dcc.Slider(id = 'id_prectot_mean',
+    min = 0.5,
+    max = 5.0 + 1 / 10**5,
+    value = 2.18,
+    step = 0.01,
+    marks = {i / 10 + 1 / 10**5: {'label': '{}mm'.format(i / 10)} for i in range(5, 51, 5)})
+
+slider5 = dcc.Slider(id = 'id_rh2m_mean',
+    min = 70,
+    max = 85,
+    value = 76.51,
+    step = 0.01,
+    marks = {i: {'label': '{}%'.format(i)} for i in range(70, 86, 3)})
+
+slider6 = dcc.Slider(id = 'id_heat_days',
+    min = 0,
+    max = 9,
+    value = 2,
+    step = 1,
+    marks = {i: {'label': '{}'.format(i)} for i in range(0, 10, 1)})
+
+# Dropdown for demographic variables
+dropdown3 = dcc.Dropdown(id = "id_region",
+                        options = [
+                            {"label": 'Antwerp', 'value': 2000},
+                            {"label": 'Dinant', 'value': 3000},
+                            {"label": 'Brussels', 'value': 4000}
+                        ],
+                        value = 4000)
+
+dropdown4 = dcc.Dropdown(id = "id_cod",
+                        options = [
+                            {"label": 'Diseases of the circulatory system', 'value': 'Diseases of the circulatory system'},
+                            {"label": 'Diseases of the respiratory system', 'value': 'Diseases of the respiratory system'},
+                            {"label": 'Diseases of the skin and subcutaneous tissue', 'value': 'Diseases of the skin and subcutaneous tissue'}
+                        ],
+                        value = 'Diseases of the circulatory system')
+
+# Cross-countries visualization objects
+country_visuals = country_plots(heatdays)
+
+# Model prediction objects
+prediction_visuals = prediction_table(model)
+
+
+# app layout
+app.layout = dbc.Container([
+        html.Div(children = [html.H1(children = 'Visualisations for European Countries',
+                                   style = {'textAlign': 'center'}),
+                           html.H2(children = 'Overall Visualisations',
+                                   style = {'textAlign': 'center'})],
+                 style = {'color': 'black'}),
+        html.Hr(),
+        html.Div([html.P('Worldwide, it is highly acknowledged that the climate is warming.'
+                         'According to the international disasters database (EM-DAT), there have been 25 '
+                         'events of heatwaves from 2018 to 2020 in Europe which were considered as natural '
+                         'disasters and 5 of them were from Belgium. Based on the data from EM-DAT, we selected 8 countries '
+                         'in Europe which were affected the most by heat waves namely Belgium, Bulgaria, France, Greece, '
+                         'Romania, Russia, Spain and United Kingdom (U.K.).')]),
+        dbc.Row([
+            dbc.Col(dcc.Graph(id = "id_graph", figure = visuals.boxplot()), md = 20),
+            dbc.Col(dcc.Graph(id = "id_graph3", figure = visuals.lineplot()), md = 20)
+            ], justify = "center"),
+        dbc.Row([
+            dbc.Col(dcc.Graph(id = "id_graph2", figure = visuals.scatterplot()), width = 8)
+        ], justify = 'center'),
+        dbc.Row([
+            dbc.Col(html.H1(children = "Country Level Visualisations",
+                            style = {'textAlign': 'center'}))
+            ],
+            align = "center"),
+        dbc.Row([
+            dbc.Col(html.Hr())
+            ]),
+        dbc.Row([
+            dbc.Col(html.P('Write summary about the country level charts'))
+        ]),
+        dbc.Row([
+            dbc.Col(dropdown, md = 4)],
+            justify = "center"),
+        dbc.Row([
+            dbc.Col(dcc.Graph(id = "id_graph4", figure = country_visuals.heatmap('Belgium'))),
+            dbc.Col(dcc.Graph(id = "id_graph5", figure = country_visuals.lineplot('Belgium')))
+        ]),
+        dbc.Row([
+            dbc.Col(html.H1(children = "Mortality model",
+                            style = {'textAlign': 'center'}))
+            ],
+            align = "center"),
+        html.Hr(),
+        dbc.Row([
+            dbc.Col(html.P('The monthly average of maximum temperature'), md = 4),
+            dbc.Col(slider1, md = 4)],
+            justify = "center"),
+        dbc.Row([
+            dbc.Col(html.P('The monthly average of temperature range'), md = 4),
+            dbc.Col(slider2, md = 4)],
+            justify = "center"),     
+        dbc.Row([
+            dbc.Col(html.P('The monthly average of wind speed'), md = 4),
+            dbc.Col(slider3, md = 4)],
+            justify = "center"),
+        dbc.Row([
+            dbc.Col(html.P('The monthly average of precipitation'), md = 4),
+            dbc.Col(slider4, md = 4)],
+            justify = "center"),
+        dbc.Row([
+            dbc.Col(html.P('The monthly average of relative humidity'), md = 4),
+            dbc.Col(slider5, md = 4)],
+            justify = "center"),
+        dbc.Row([
+            dbc.Col(html.P('The number of heat days in the month'), md = 4),
+            dbc.Col(slider6, md = 4)],
+            justify = "center"),
+        dbc.Row([
+            dbc.Col(html.P('Region'), md = 4),
+            dbc.Col(dropdown3, md = 4)],
+            justify = "center"),
+        dbc.Row([
+            dbc.Col(html.P('Cause of death'), md = 4),
+            dbc.Col(dropdown4, md = 4)],
+            justify = "center"),
+        dbc.Row([
+            dbc.Col(dcc.Graph(id = "id_graph6", figure = prediction_visuals.draw_table(data_pred)))
+            ])
+        ],
+        fluid = True,)
+
+@app.callback(
+    Output('id_graph4', 'figure'),
+    Output('id_graph5', 'figure'),
+    Output('id_graph6', 'figure'),
+    [Input('id_country', 'value'),
+     Input('id_temp_mean', 'value'),
+     Input('id_temp_rng', 'value'),
+     Input('id_ws50m_mean', 'value'),
+     Input('id_prectot_mean', 'value'),
+     Input('id_rh2m_mean', 'value'),
+     Input('id_heat_days', 'value'),
+     Input('id_region', 'value'),
+     Input('id_cod', 'value')]
+)
+
+def update_chart(id_country, id_temp_mean, id_temp_rng, id_ws50m_mean, id_prectot_mean, 
+                 id_rh2m_mean, id_heat_days, id_region, id_cod):
+    fig1 = country_visuals.heatmap(id_country)
+    fig2 = country_visuals.lineplot(id_country)
+    
+    data_pred = {'TEMP_MEAN': id_temp_mean, 'TEMP_RNG': id_temp_rng, 'WS50M_MEAN': id_ws50m_mean, 
+                 'PRECTOT_MEAN': id_prectot_mean, 'RH2M_MEAN': id_rh2m_mean, 'HEAT_DAYS': id_heat_days, 
+                 'YEAR': 2018, 'MONTH': 8, 'REGION': id_region, 'COD': id_cod}
+    fig3 = prediction_visuals.draw_table(data_pred)
+    return fig1, fig2, fig3
+
+if __name__ == '__main__':
+    app.run_server(debug = True)
---
+++ graphs.py
@@ -0,0 +1,95 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+# considering only summer months from May to August
+class subset_helper():
+    def __init__(self, data):
+        self.data = data
+
+    def get_summer(self, df):
+        return df[(df['MONTH'] <= 8) & (df['MONTH'] >= 5)]
+
+    def get_year(self, df):
+        return df[(df['YEAR'] < 2021) & (df['YEAR'] > 2008)]
+
+    def subset(self):
+        self.data = self.get_year(self.get_summer(self.data))
+        return self.data
+
+
+# overall visualizations for all 8 countries
+class overall_plots():
+    def __init__(self, df_concat):
+        self.data = df_concat
+
+    def boxplot(self):
+        plt.figure(figsize=(8, 5), dpi=80)
+        ax = px.box(self.data,x="COUNTRY", y="TEMP_MEAN",color='COUNTRY',
+                    title = "Distribution of daily maximum temperature across different countries")
+        return ax
+
+    def scatterplot(self):
+        df_group = self.data.groupby(['YEAR', 'COUNTRY']).mean()
+        df_group = df_group.reset_index().round(2)
+        fig = px.scatter(df_group, x="YEAR", y="HEAT_DAYS", color="COUNTRY", size="HEAT_DAYS",
+                         title="Heat days of countries for period 2009-2020")
+        return fig
+
+    def lineplot(self):
+        df_group = self.data.groupby(['YEAR', 'COUNTRY']).mean()
+        df_group = df_group.reset_index().round(2)
+        fig = px.line(df_group, x="YEAR", y="TEMP_MEAN", color="COUNTRY",
+                      title='Temperature of countries for period 2009-2020')
+        return fig
+
+
+# country specific visuals
+class country_plots():
+    def __init__(self, data):
+        self.data = data
+
+    def heatmap(self,country):
+        data =  self.data.loc[self.data['COUNTRY'] == country]
+        df = data.groupby(['MONTH', 'YEAR']).mean()
+        df = df.reset_index().round(2)
+        df = df.pivot("MONTH", "YEAR", "HEAT_DAYS")
+        df = df.fillna(0)
+        fig = px.imshow(df, labels=dict(x="YEAR", y="MONTH", color="HEAT_DAYS"))
+        fig.update_layout(yaxis_nticks=5, title="Heat Days per Year")
+        return fig
+
+    def lineplot(self,country):
+        data = self.data.loc[self.data['COUNTRY'] == country]
+        fig = px.line(data, x="MONTH", y="HEAT_DAYS", facet_col="YEAR", facet_col_wrap=4,
+                      title="Monthly distribution")
+        return fig
+    
+# model prediction
+class prediction_table():
+    def __init__(self,model):
+        self.model = model
+        
+    def draw_table(self,data):
+        data_ages = pd.DataFrame()
+        ages = ['0-24', '25-44', '45-64', '65-74', '75-84', '85+']
+        genders = ['F', 'M']
+        # Insert possible ages
+        for age in ages:
+            data['AGE-GROUP'] = age
+            for gender in genders:
+                data['SEX'] = gender
+                data_ages = data_ages.append(data, ignore_index=True)
+        # Reorder the features
+        data_ages = data_ages[['REGION', 'MONTH', 'YEAR', 'AGE-GROUP', 'SEX', 'COD', 'TEMP_MEAN', 'TEMP_RNG', 
+                             'WS50M_MEAN', 'PRECTOT_MEAN', 'RH2M_MEAN', 'HEAT_DAYS']]
+        # Dataframe for plotting
+        output = pd.DataFrame()
+        output['Gender'] = data_ages['SEX']
+        output['Age groups'] = data_ages['AGE-GROUP']
+        # Model prediction
+        output['Predicted mortality'] = self.model.predict(data_ages)
+        # Plot
+        fig = px.bar(output, y='Age groups', x='Predicted mortality',
+                     color='Gender', title='Mortality prediction', barmode='group', orientation='h')
+        return fig
